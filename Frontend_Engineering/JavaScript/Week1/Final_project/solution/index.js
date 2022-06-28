854c8bb4a499f05386e72aa45662a5aac9281c0e
Divine Orji <dpkreativ@gmail.com>
2022-06-28 08:32:04 +0100

    chore: a little housekeeping

--- Frontend_Engineering/JavaScript/Week1/Final_project/solution/index.js
+++ Frontend_Engineering/JavaScript/Week1/Final_project/solution/index.js
@@ -13,6 +13,61 @@ In this last section, we'll consolidate all you have learnt this week, with some
 
 */
 
+function getUserDetails() {
+  let username = prompt('Input username');
+
+  while (!validateUsername(username)) {
+    username.length > 10
+      ? (username = prompt('Too long. Make your username shorter'))
+      : (username = prompt('Invalid. Please pick a username'));
+  }
+
+  let password = getPassword();
+
+  while (!validatePassword(password)) {
+    password.length < 6
+      ? (password = getPassword(
+          'Too short. Make it longer (6 - 15 characters)'
+        ))
+      : (password = getPassword(
+          'Too long. Make it shorter (6 - 15 characters)'
+        ));
+  }
+
+  let confirmPassword = prompt('Confirm password');
+
+  while (!validatePassword(confirmPassword) && confirmPassword !== password) {
+    password = getPassword(
+      `Passwords don't match. Start over with a new password`
+    );
+    confirmPassword = prompt('Confirm password');
+  }
+
+  const user = {
+    username: username,
+    password: password,
+  };
+
+  console.log(user);
+
+  function validateUsername(x) {
+    if (x === null) alert('You have ended this session');
+    return x !== '' && x.length < 10;
+  }
+
+  function getPassword(content = 'Input password') {
+    let pwd = prompt(content);
+    return pwd;
+  }
+
+  function validatePassword(y) {
+    if (y === null) alert('You have ended this session');
+    return y.length >= 6 && y.length <= 15;
+  }
+}
+
+getUserDetails();
+
 // // Object for storing user details
 // const user = {
 //     username: '',
@@ -69,58 +124,3 @@ In this last section, we'll consolidate all you have learnt this week, with some
 
 // // Print user details to console
 // console.log(user);
-
-function getUserDetails() {
-  let username = prompt('Input username');
-
-  while (!validateUsername(username)) {
-    username.length > 10
-      ? (username = prompt('Too long. Make your username shorter'))
-      : (username = prompt('Invalid. Please pick a username'));
-  }
-
-  let password = getPassword();
-
-  while (!validatePassword(password)) {
-    password.length < 6
-      ? (password = getPassword(
-          'Too short. Make it longer (6 - 15 characters)'
-        ))
-      : (password = getPassword(
-          'Too long. Make it shorter (6 - 15 characters)'
-        ));
-  }
-
-  let confirmPassword = prompt('Confirm password');
-
-  while (!validatePassword(confirmPassword) && confirmPassword !== password) {
-    password = getPassword(
-      `Passwords don't match. Start over with a new password`
-    );
-    confirmPassword = prompt('Confirm password');
-  }
-
-  const user = {
-    username: username,
-    password: password,
-  };
-
-  console.log(user);
-
-  function validateUsername(x) {
-    if (x === null) alert('You have ended this session');
-    return x !== '' && x.length < 10;
-  }
-
-  function getPassword(content = 'Input password') {
-    let pwd = prompt(content);
-    return pwd;
-  }
-
-  function validatePassword(y) {
-    if (y === null) alert('You have ended this session');
-    return y.length >= 6 && y.length <= 15;
-  }
-}
-
-getUserDetails();
